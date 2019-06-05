93585c5362e259909bc4b4992cfa5dee888c2999
Whalysson A. Aguiar <whalyssonaguiar@hotmail.com>
2019-06-04 21:29:48 -0300

    Classe Figura

---
+++ Github/Princiapal/Figura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Princiapal
+{
+    abstract class Figura
+    {
+        private string nome;
+        private string cor;
+
+        Figura() { }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+
+        abstract public double Area();
+
+
+    }
+}
